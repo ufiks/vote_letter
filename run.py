6b1d11c8f5d9c22201e85b68a9e14a78c4d1cbe5
maker <makerovilja@mail.ru>
2021-09-12 23:49:51 +0300

    oops

--- run.py
+++ run.py
@@ -2,12 +2,12 @@ from flask import Flask
 import json
 
 app = Flask(__name__)
-lettersall = json.dumps({'A': 1, 'B': 2, 'C': 3, 'D': 4})
 
 
 @app.route('/letters', methods=['Get'])
 def get_letter():
-    return lettersall
+    letters_all = json.dumps({'A': 1, 'B': 2, 'C': 3, 'D': 4})
+    return letters_all
 
 
 @app.route('/letters', methods=['post'])
