fc5eb6fe02b935913e553a37dca73790d6a5fd8d
maker <makerovilja@mail.ru>
2021-09-23 00:37:51 +0300

    check this

--- run.py
+++ run.py
@@ -1,26 +1,55 @@
-from flask import Flask
+from flask import Flask, request, abort
 import json
+import redis
 
 app = Flask(__name__)
+redis = redis.Redis(host='127.0.0.1')
+all_letters = {
+    'A': 0,
+    'B': 0,
+    'C': 0,
+    'D': 0
+}
+
+
+def get_all_letters(*args):
+    value = redis.get(*args)
+    letters_decode = value.decode("utf-8")
+    new_letters = json.loads(letters_decode.replace("'", '"'))
+    return new_letters
 
 
 @app.route('/letters', methods=['Get'])
-def get_letter():
-    letters_all = json.dumps({'A': 1, 'B': 2, 'C': 3, 'D': 4})
-    return letters_all
-
-
-@app.route('/letters', methods=['post'])
 def letters():
-    letters_1 = json.dumps('letter:A')
-    return letters_1
+    if request.args.get('letter'):
+        name_letter = get_all_letters('all_letters')
+        dels = name_letter[request.args.get('letter')]
+        return {'numberOfVotes': dels }, 200
+    else:
+        del_letters = get_all_letters('all_letters')
+        return del_letters, 200
+
+
+@app.route('/vote', methods=['Post'])
+def vote():
+    body = json.loads(request.data)
+    letters_in_redis = get_all_letters('all_letters')
+    if 'letter' not in letters_in_redis:
+        abort(400, 'letter not detected')
+    if body.get('letter') in letters_in_redis:
+        current = letters_in_redis[request.args.get('letter')]
+        current += 1
+        redis.set(request.args.get('letter'), current)
+        return json.dumps(letters_in_redis)
 
 
 @app.route('/topletters', methods=['Get'])
 def top_letters():
-    topletters = json.dumps('topletter: B')
-    return topletters
+    topletter = get_all_letters('all_letters')
+    return json.dumps(topletter), 200
 
+
+redis.set('all_letters', str(all_letters))
 
 if __name__ == '__main__':
     app.run(debug=True)
